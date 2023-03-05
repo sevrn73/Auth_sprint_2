be957f716ee93e42cad412fb80ecc1b8c3058f13
sevrn73 <57312403+sevrn73@users.noreply.github.com>
2023-03-05 23:31:53 +0300

    f

--- fastapi/src/auth_bearer.py
+++ fastapi/src/auth_bearer.py
@@ -1,4 +1,5 @@
 import httpx
+from http import HTTPStatus
 from fastapi import Request, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
@@ -10,22 +11,22 @@ class JWTBearer(HTTPBearer):
     async def __call__(self, request: Request):
         credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
         if credentials:
-            if not credentials.scheme == 'Bearer':
-                raise HTTPException(status_code=403, detail='Invalid authentication scheme.')
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail="Invalid authentication scheme.")
 
-            isTokenValid = await self.verify_jwt(credentials.credentials)
-            if not isTokenValid:
-                raise HTTPException(status_code=401, detail='Invalid token or expired token.')
+            is_token_valid = await self.verify_jwt(credentials.credentials)
+            if not is_token_valid:
+                raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="Invalid token or expired token.")
             return credentials.credentials
         else:
-            raise HTTPException(status_code=403, detail='Invalid authorization code.')
+            raise HTTPException(status_code=HTTPStatus.FORBIDDEN, detail="Invalid authorization code.")
 
     async def verify_jwt(self, jwtoken: str) -> bool:
         async with httpx.AsyncClient() as client:
             response = await client.get(
-                'http://nginx:80/v1/check_perm', headers={'Authorization': 'Bearer ' + jwtoken}
+                "http://nginx:80/v1/check_perm", headers={"Authorization": "Bearer " + jwtoken}
             )
-        if response.status_code == 200:
+        if response.status_code == HTTPStatus.OK:
             return True
         else:
             return False
--- fastapi/src/main.py
+++ fastapi/src/main.py
@@ -10,41 +10,41 @@ from db import elastic, redis
 
 app = FastAPI(
     title=ProjectSettings().PROJECT_NAME,
-    docs_url='/api/openapi',
-    openapi_url='/api/openapi.json',
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
     default_response_class=ORJSONResponse,
-    description='Информация о фильмах, жанрах и людях, участвовавших в создании произведения',
-    version='1.0.0',
+    description="Информация о фильмах, жанрах и людях, участвовавших в создании произведения",
+    version="1.0.0",
 )
 
 
-@app.on_event('startup')
+@app.on_event("startup")
 async def startup():
     redis.redis = await aioredis.create_redis_pool(
         (ProjectSettings().REDIS_HOST, ProjectSettings().REDIS_PORT), minsize=10, maxsize=20
     )
     elastic.es = AsyncElasticsearch(
-        hosts=[f'{ProjectSettings().ELASTIC_HOST}:{ProjectSettings().ELASTIC_PORT}'],
+        hosts=[f"{ProjectSettings().ELASTIC_HOST}:{ProjectSettings().ELASTIC_PORT}"],
         basic_auth=(ProjectSettings().ES_USER, ProjectSettings().ES_PASSWORD),
         verify_certs=False,
     )
 
 
-@app.on_event('shutdown')
+@app.on_event("shutdown")
 async def shutdown():
     redis.redis.close()
     await redis.redis.wait_closed()
     await elastic.es.close()
 
 
-app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
-app.include_router(genres.router, prefix='/api/v1/genres', tags=['genres'])
-app.include_router(persons.router, prefix='/api/v1/persons', tags=['persons'])
+app.include_router(films.router, prefix="/api/v1/films", tags=["films"])
+app.include_router(genres.router, prefix="/api/v1/genres", tags=["genres"])
+app.include_router(persons.router, prefix="/api/v1/persons", tags=["persons"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(
-        'main:app',
-        host='0.0.0.0',
+        "main:app",
+        host="0.0.0.0",
         port=8001,
     )
